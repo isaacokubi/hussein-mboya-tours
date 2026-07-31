ca3289c93e68a4e574c2960712296cbae9d9a244
Barack Okubi <izobrack3@gmail.com>
2026-07-31 11:48:34 +0300

    Fix withTransaction import

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -19,7 +19,7 @@ import { calculateBookingAmounts } from "../utils/bookingPricing.js";
 
 import { successResponse, errorResponse } from "../utils/apiResponse.js";
 
-import { withTransaction } from "../utils/withTransactions.js";
+import { withTransaction } from "../utils/withTransaction.js";
 
 /*
 |--------------------------------------------------------------------------
