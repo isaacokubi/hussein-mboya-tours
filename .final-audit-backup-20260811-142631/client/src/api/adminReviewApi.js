ccb5740e0898321ecfc57153edba500a80cc21a8
Barack Okubi <izobrack3@gmail.com>
2026-08-11 20:30:13 +0300

    Remove backup and duplicate project files

--- .final-audit-backup-20260811-142631/client/src/api/adminReviewApi.js
+++
@@ -1,6 +0,0 @@
-import api from "./axios";
-
-export const getAdminReviews = async () => (await api.get("/admin/reviews")).data;
-export const approveReview = async (id) => (await api.patch(`/admin/reviews/${id}/approve`)).data;
-export const rejectReview = async (id, reason = "") => (await api.patch(`/admin/reviews/${id}/reject`, { reason })).data;
-export const deleteReview = async (id) => (await api.delete(`/admin/reviews/${id}`)).data;
--- .final-audit-backup-20260811-142631/server/middleware/guideMiddleware.js
+++
@@ -1,6 +0,0 @@
-import { roleMiddleware } from "./roleMiddleware.js";
-
-const guideMiddleware = roleMiddleware("tour_guide", "guide");
-
-export { guideMiddleware };
-export default guideMiddleware;
