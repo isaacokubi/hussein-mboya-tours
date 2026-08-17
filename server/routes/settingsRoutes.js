b8c512b08451d98e34b4d767eb49e36188152508
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-08-17 12:10:44 +0300

    chore: remove obsolete RBAC placeholder

--- server/routes/settingsRoutes.js
+++ server/routes/settingsRoutes.js
@@ -1,32 +1,14 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
 import { authorize } from "../middleware/permissionMiddleware.js";
-import {
-  getSettings,
-  updateSettings
-} from "../controllers/settingsController.js";
+import { getSettings, updateSettings } from "../controllers/settingsController.js";
 
 const router = express.Router();
 
 router.use(protect);
 router.use(authorize("settings.manage"));
 
-
-// Get system settings
-router.get(
-  "/",
-  getSettings
-);
-
-
-// Update system settings
-router.put(
-  "/",
-  updateSettings
-);
-
+router.get("/", getSettings);
+router.put("/", updateSettings);
 
 export default router;
-
-
-// RBAC middleware placeholder
