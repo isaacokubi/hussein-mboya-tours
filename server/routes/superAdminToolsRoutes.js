0a2df48789a0ec437bbef0c429173dfd311f24f0
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-08-17 12:10:50 +0300

    security: remove obsolete RBAC placeholder

--- server/routes/superAdminToolsRoutes.js
+++ server/routes/superAdminToolsRoutes.js
@@ -1,27 +1,19 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
 import { authorize } from "../middleware/permissionMiddleware.js";
-
 import {
-getDatabase,
-getApiMonitor,
-getSystem,
-getSettings
+  getDatabase,
+  getApiMonitor,
+  getSystem,
+  getSettings,
 } from "../controllers/superAdminToolsController.js";
 
-
-const router=express.Router();
+const router = express.Router();
 
 router.use(protect);
 router.use(authorize("system.security"));
 
-
-// security endpoint handled by superAdminOperationsRoutes
-router.get("/database",getDatabase);
-router.get("/settings",getSettings);
-
+router.get("/database", getDatabase);
+router.get("/settings", getSettings);
 
 export default router;
-
-
-// RBAC middleware placeholder
