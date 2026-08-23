9faab3869686182b2f390bf427ab0b284a89137a
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-08-23 18:19:08 +0300

    Route customer bookings through tenant-safe dashboard feed

--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -2,7 +2,8 @@ import { resolveTenant } from "../middleware/tenantMiddleware.js";
 // server/routes/bookingRoutes.js
 
 import express from "express";
-import { getMyBookings, getBooking, cancelBooking, rescheduleBooking, getAllBookings, getConfirmedBookings, updateBookingStatus } from "../controllers/bookingController.js";
+import { getBooking, cancelBooking, rescheduleBooking, getAllBookings, getConfirmedBookings, updateBookingStatus } from "../controllers/bookingController.js";
+import { getCustomerBookings } from "../controllers/customerDashboardController.js";
 import { updateBookingTravelDate } from "../controllers/bookingTravelDateController.js";
 import { createCustomerBooking } from "../controllers/customerBookingController.js";
 import { protect, customerOnly, adminOnly, managerOnly } from "../middleware/authMiddleware.js";
@@ -15,7 +16,7 @@ router.use(resolveTenant);
 router.use(protect);
 router.get("/test", (req, res) => res.status(200).json({ success: true, message: "Booking routes are loaded" }));
 router.post("/", customerOnly, validateBookingTravelDate, createCustomerBooking);
-router.get("/my-bookings", customerOnly, getMyBookings);
+router.get("/my-bookings", customerOnly, getCustomerBookings);
 router.put("/cancel/:id", customerOnly, cancelBooking);
 router.put("/reschedule/:id", customerOnly, rescheduleBooking);
 router.put("/:id/travel-date", customerOnly, updateBookingTravelDate);
