18fbc717813836617d054c5266e28b063a56c16a
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-08-24 02:05:25 +0300

    Protect tenant branding updates

--- server/routes/tenantBrandingRoutes.js
+++ server/routes/tenantBrandingRoutes.js
@@ -1,23 +1,13 @@
 import express from "express";
-import {
-    getBranding,
-    updateBranding
-} from "../controllers/tenantBrandingController.js";
-
+import { getBranding, updateBranding } from "../controllers/tenantBrandingController.js";
+import { protect, requireRoles } from "../middleware/authMiddleware.js";
 import { resolveTenant } from "../middleware/tenantMiddleware.js";
 
 const router = express.Router();
 
 router.use(resolveTenant);
 
-router.get(
-    "/",
-    getBranding
-);
-
-router.put(
-    "/",
-    updateBranding
-);
+router.get("/", getBranding);
+router.put("/", protect, requireRoles("admin", "manager"), updateBranding);
 
 export default router;
