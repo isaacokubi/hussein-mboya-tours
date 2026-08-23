4461b38c2b9c7b50ba4d4bba2eed2a86021ce064
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-08-23 18:26:19 +0300

    Add SuperAdmin tenant management API client

---
+++ client/src/api/superAdminTenantsApi.js
@@ -0,0 +1,6 @@
+import axios from "./axios";
+
+export const getSuperAdminTenants = async (params = {}) => (await axios.get("/superadmin/tenants", { params })).data;
+export const getSuperAdminTenant = async (id) => (await axios.get(`/superadmin/tenants/${id}`)).data;
+export const updateSuperAdminTenantStatus = async (id, status) => (await axios.patch(`/superadmin/tenants/${id}/status`, { status })).data;
+export const deleteSuperAdminTenant = async (id, confirmation) => (await axios.delete(`/superadmin/tenants/${id}`, { data: { confirmation } })).data;
