192f1946bf73abaa39336950cc21406d00c3a5af
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-09-04 20:34:55 +0300

    fix: correct admin booking controller imports for Render startup

--- server/routes/adminBookingRoutes.js
+++ server/routes/adminBookingRoutes.js
@@ -1,19 +1,32 @@
+import express from "express";
+
 import { resolveTenant } from "../middleware/tenantMiddleware.js";
 import { authorize } from "../middleware/permissionMiddleware.js";
-import express from "express";
-import { getBookings, getBooking, updateBookingStatus, assignResources, updatePaymentStatus, getBookingTimeline, downloadBookingInvoice, sendBookingNotification } from "../controllers/bookingAdminController.js";
 import { protect } from "../middleware/authMiddleware.js";
 import adminMiddleware from "../middleware/adminMiddleware.js";
 import { guardBookingResources } from "../middleware/resourceTenantGuard.js";
 
+import {
+  getAllBookings,
+  getBookingById,
+  updateBookingStatus,
+  assignResources,
+  updatePaymentStatus,
+  getBookingTimeline,
+  downloadBookingInvoice,
+} from "../controllers/adminBookingController.js";
+
+import { sendBookingNotification } from "../controllers/notificationController.js";
+
 const router = express.Router();
+
 router.use(resolveTenant);
 router.use(protect);
 router.use(adminMiddleware);
 router.use(authorize("booking.manage"));
 
-router.get("/", getBookings);
-router.get("/:id", getBooking);
+router.get("/", getAllBookings);
+router.get("/:id", getBookingById);
 router.put("/:id/status", updateBookingStatus);
 router.put("/:id/assign", guardBookingResources, assignResources);
 router.put("/:id/payment", updatePaymentStatus);
