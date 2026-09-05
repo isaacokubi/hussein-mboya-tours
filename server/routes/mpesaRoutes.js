0864f8439c8678b2fec19efb9e816f78bff41ae3
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-09-05 12:54:27 +0300

    feat: expose M-Pesa B2C payout callbacks

--- server/routes/mpesaRoutes.js
+++ server/routes/mpesaRoutes.js
@@ -3,12 +3,18 @@ import express from "express";
 import { mpesaRefundResult, mpesaRefundTimeout } from "../controllers/mpesaRefundController.js";
 import { stkPush, checkCheckoutStatus, verifyBookingPayment } from "../controllers/mpesaController.js";
 import { subscriptionMpesaCallback } from "../controllers/subscriptionMpesaCallbackController.js";
+import { mpesaB2cResult, mpesaB2cTimeout } from "../controllers/mpesaB2cController.js";
 import { protect } from "../middleware/authMiddleware.js";
 import { prepareBookingForMpesa } from "../middleware/prepareBookingForMpesa.js";
 import { resolveMpesaCallbackTenant } from "../middleware/resolveMpesaCallbackTenant.js";
 import { resolveMpesaRefundTenant } from "../middleware/resolveMpesaRefundTenant.js";
 
 const router = express.Router();
+
+// Safaricom calls these server-to-server endpoints without the user's JWT.
+router.post("/b2c/result", mpesaB2cResult);
+router.post("/b2c/timeout", mpesaB2cTimeout);
+
 router.use(resolveTenant);
 router.post("/stkpush", protect, prepareBookingForMpesa, stkPush);
 router.post("/mpesa", protect, prepareBookingForMpesa, stkPush);
