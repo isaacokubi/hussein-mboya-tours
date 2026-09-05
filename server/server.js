56b2166b780dd3b6deedb7affdda3fefa3b9a103
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-09-05 14:12:24 +0300

    Remove late subscription route mount

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ import systemHealthRoutes from "./routes/systemHealthRoutes.js";
 import databaseRoutes from "./routes/databaseRoutes.js";
 import settingsRoutes from "./routes/settingsRoutes.js";
 import superAdminRoutes from "./routes/superAdminRoutes.js";
-import tenantSubscriptionRoutes from "./routes/tenantSubscriptionRoutes.js";
 import http from "http";
 import mongoose from "mongoose";
 import app from "./app.js";
@@ -31,7 +30,6 @@ app.use("/api/settings", settingsRoutes);
 app.use("/api/database", databaseRoutes);
 app.use("/api/system", systemHealthRoutes);
 app.use("/api/superadmin", superAdminRoutes);
-app.use("/api/subscription", tenantSubscriptionRoutes);
 
 const shutdown = async (exitCode = 0) => {
   clearInterval(lifecycleInterval);
