ae092520b321c7a6f6bb637f1d1ab4ba3c56d6fa
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-09-09 14:48:23 +0300

    feat(etims): expose manual invoice queue endpoint

--- server/routes/complianceRoutes.js
+++ server/routes/complianceRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { listComplianceRecords, upsertComplianceRecord, getComplianceSummary } from "../controllers/complianceController.js";
+import { listComplianceRecords, upsertComplianceRecord, getComplianceSummary, queueEtimsInvoice } from "../controllers/complianceController.js";
 import { resolveTenant } from "../middleware/tenantMiddleware.js";
 import { protect } from "../middleware/authMiddleware.js";
 import { authorize } from "../middleware/permissionMiddleware.js";
@@ -9,4 +9,5 @@ router.use(resolveTenant, protect);
 router.get("/", authorize("finance.view"), listComplianceRecords);
 router.get("/summary", authorize("finance.view"), getComplianceSummary);
 router.put("/:type", authorize("finance.manage"), (req, res, next) => { req.body = { ...(req.body || {}), type: req.params.type }; return upsertComplianceRecord(req, res, next); });
+router.post("/etims/invoices/:id/queue", authorize("finance.manage"), queueEtimsInvoice);
 export default router;
