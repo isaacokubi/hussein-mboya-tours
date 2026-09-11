fa09cdba6f9a5bfe7c1d38540aaa605a5c53d411
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-09-11 21:35:44 +0300

    use hardened hospitality admin reads

--- server/routes/hotelRoutes.js
+++ server/routes/hotelRoutes.js
@@ -1,19 +1,20 @@
 import express from "express";
 import { protect, managerOnly } from "../middleware/authMiddleware.js";
-import { listHotels, getHotel, listAdminHotels, createHotel, updateHotel, createRoomType, updateRoomType, createHotelBooking, listHotelBookings, updateHotelBooking } from "../controllers/hotelController.js";
+import { listHotels, getHotel, createHotel, updateHotel, createRoomType, updateRoomType, createHotelBooking, updateHotelBooking } from "../controllers/hotelController.js";
 import { checkHotelAvailability } from "../controllers/hotelAvailabilityController.js";
 import { createEnhancedHotelBooking } from "../controllers/enhancedHotelBookingController.js";
+import { listAdminHotelsSafe, listHotelBookingsSafe } from "../controllers/hospitalityAdminController.js";
 
 const router = express.Router();
 router.get("/", listHotels);
 router.get("/availability", checkHotelAvailability);
-router.get("/admin/catalog", protect, managerOnly, listAdminHotels);
+router.get("/admin/catalog", protect, managerOnly, listAdminHotelsSafe);
 router.post("/admin/catalog", protect, managerOnly, createHotel);
 router.patch("/admin/catalog/:id", protect, managerOnly, updateHotel);
 router.post("/admin/catalog/:hotelId/rooms", protect, managerOnly, createRoomType);
 router.patch("/admin/rooms/:id", protect, managerOnly, updateRoomType);
 router.post("/bookings", protect, createEnhancedHotelBooking);
-router.get("/bookings", protect, listHotelBookings);
+router.get("/bookings", protect, listHotelBookingsSafe);
 router.patch("/bookings/:id", protect, updateHotelBooking);
 router.get("/:id", getHotel);
 export default router;
