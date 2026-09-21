1012144c6295d32952c0a65e87ab94246dac2d9f
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-09-22 00:18:27 +0300

    fix: normalize transfer booking tenant plugin registration

--- server/models/AirportTransferBooking.js
+++ server/models/AirportTransferBooking.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 import { tenantPlugin } from "../tenancy/tenantPlugin.js";
-import { tenantPlugin } from "../tenancy/tenantPlugin.js";
 const { Schema } = mongoose;
 
 const AirportTransferBookingSchema = new Schema({
@@ -49,8 +48,6 @@ const AirportTransferBookingSchema = new Schema({
 AirportTransferBookingSchema.index({ tenantId: 1, reference: 1 }, { unique: true });
 AirportTransferBookingSchema.index({ tenantId: 1, pickupDateTime: 1, status: 1 });
 AirportTransferBookingSchema.index({ tenantId: 1, customer: 1, createdAt: -1 });
-
-AirportTransferBookingSchema.plugin(tenantPlugin);
 
 AirportTransferBookingSchema.plugin(tenantPlugin);
 
