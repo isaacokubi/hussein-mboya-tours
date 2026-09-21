960c797d60c012ed169e3121c00d7c1ad972e211
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-09-22 00:17:47 +0300

    fix: remove duplicate tenant plugin registration

--- server/models/AccommodationInventory.js
+++ server/models/AccommodationInventory.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 import { tenantPlugin } from "../tenancy/tenantPlugin.js";
-import { tenantPlugin } from "../tenancy/tenantPlugin.js";
 const { Schema } = mongoose;
 const AccommodationInventorySchema = new Schema({
   tenantId:{type:Schema.Types.ObjectId,ref:"Organization",required:true,index:true},
@@ -17,8 +16,6 @@ const AccommodationInventorySchema = new Schema({
   updatedBy:{type:Schema.Types.ObjectId,ref:"User"},
 },{timestamps:true});
 AccommodationInventorySchema.index({tenantId:1,propertyName:1,roomType:1},{unique:true});
-AccommodationInventorySchema.plugin(tenantPlugin);
-
 AccommodationInventorySchema.plugin(tenantPlugin);
 
 export default mongoose.models.AccommodationInventory || mongoose.model("AccommodationInventory",AccommodationInventorySchema);
