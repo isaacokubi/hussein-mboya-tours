f1bdcba0297720e142d69c2fd40ad408b84d1f8a
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-09-22 23:22:02 +0300

    refactor: migrate operational seed/script to Firebase

--- server/scripts/repairGlobalToursMedia.js
+++ server/scripts/repairGlobalToursMedia.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import * as firestore from "../config/firestore.js";
 import dotenv from "dotenv";
 import Destination from "../models/Destination.js";
 import Tour from "../models/Tour.js";
@@ -70,8 +70,8 @@ const hasUsableImage = (value) => {
 };
 
 async function repair() {
-  if (!process.env.MONGODB_URI) throw new Error("MONGODB_URI is missing in .env");
-  await mongoose.connect(process.env.MONGODB_URI);
+  if (!process.env.FIREBASE_PROJECT_ID) throw new Error("MONGODB_URI is missing in .env");
+  await firestore.connectFirestore?.();
 
   await runWithTenant({ role: "super_admin", bypass: true }, async () => {
     const destinations = await Destination.find({ name: { $in: DESTINATIONS }, isDeleted: false }).sort({ createdAt: 1 });
@@ -205,5 +205,5 @@ repair()
     process.exitCode = 1;
   })
   .finally(async () => {
-    await mongoose.connection.close().catch(() => {});
+    await firestore.connection.close().catch(() => {});
   });
