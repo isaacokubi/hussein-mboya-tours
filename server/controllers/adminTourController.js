789971b21550b2ef63a6b718ee3f2a3cd2ce47dd
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-09-22 23:15:32 +0300

    refactor: replace MongoDB controller references with Firebase

--- server/controllers/adminTourController.js
+++ server/controllers/adminTourController.js
@@ -1,5 +1,5 @@
 import { mergeTenantFilter , requireTenantId} from "../tenancy/context.js";
-import mongoose from "mongoose";
+import * as firestore from "../config/firestore.js";
 import { getSystemSettings } from "../services/settingsService.js";
 import Tour from "../models/Tour.js";
 import User from "../models/User.js";
@@ -37,7 +37,7 @@ const ASSIGNMENT_STATUSES = [
 |--------------------------------------------------------------------------
 */
 
-const isValidId = (id) => mongoose.Types.ObjectId.isValid(id);
+const isValidId = (id) => firestore.Types.ObjectId.isValid(id);
 
 const cleanString = (value) =>
   typeof value === "string" ? value.trim() : value;
